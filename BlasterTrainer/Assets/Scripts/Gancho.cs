0701ca2b4fe12bb586a12f14d203e2b172a2a0ae
OscarMontesinos <79588290+OscarMontesinos@users.noreply.github.com>
2022-05-12 19:27:11 +0200

    proyecto

---
+++ BlasterTrainer/Assets/Scripts/Gancho.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gancho : MonoBehaviour
+{
+    bool gancheable = true;
+    bool avanzando;
+    bool enganchado;
+    public float speed = 0;
+    public float range = 0;
+    float path = 0;
+    public GameObject point;
+    public GameObject robot;
+
+    public virtual void Awake()
+    {
+    
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && gancheable)
+        {
+            gancheable = false;
+            avanzando = true;
+            StartCoroutine(Avance());
+        }
+        if (Input.GetMouseButtonUp(1)) 
+        {
+            if (!gancheable)
+            {
+                avanzando = false;
+                enganchado = false;
+                StartCoroutine(Volver());
+            }
+        }
+        if (enganchado)
+        {
+            var dist = transform.position - robot.transform.position;
+            robot.GetComponent<PlayerMovement>()._rigidbody.AddForce( dist* robot.GetComponent<PlayerMovement>().hookTractionSpeed, ForceMode.Impulse);
+        }
+    }
+
+
+
+    public virtual IEnumerator Avance()
+    {
+        path = 0;
+        transform.parent = null;
+        while (range >= path && avanzando && !enganchado)
+        {
+
+           transform.Translate( Vector3.forward * speed*  Time.deltaTime);
+            path += speed * Time.deltaTime;
+            
+            yield return new WaitForSeconds(0.0001f);
+
+        }
+        avanzando = false;
+        if(!avanzando && !enganchado)
+        {
+            StartCoroutine(Volver());
+        }
+    }
+    public virtual IEnumerator Volver()
+    {
+        var dist = point.transform.position - transform.position;
+        while (dist.magnitude > 0.5)
+        {
+            dist = point.transform.position - transform.position;
+            //transform.Translate(dist.normalized * speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, point.transform.position, speed * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+
+        }
+        gancheable = true;
+        transform.position = point.transform.position;
+        transform.parent = point.transform;
+        transform.rotation= robot.transform.rotation;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "object")
+        {
+            enganchado = true;
+        }
+    }
+}
---
+++ BlasterTrainer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody _rigidbody;
+    public Camera cam;
+    Vector3 movement;
+    float horizontalInput = 0;
+    float verticalInput = 0;
+    public float speed;
+    public float jumpSpeed;
+    public float hookTractionSpeed;
+    public float shootRecoil;
+    public float shootTime;
+    float shootTimer;
+    public float maxSpeed;
+    public float gravityMultiplier;
+
+    public GameObject bullet;
+    public GameObject gancho;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (shootTimer > 0)
+        {
+            shootTimer -= Time.deltaTime;
+        }
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        movement = new Vector3(horizontalInput, 0, verticalInput);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+        if (Input.GetMouseButton(0) && shootTimer <=0)
+        {
+            _rigidbody.AddForce(transform.forward * -shootRecoil, ForceMode.Impulse);
+            shootTimer = shootTime;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        var wantedPos = new Vector3(0, 0, 0);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            wantedPos = raycastHit.point;
+        }
+
+        transform.LookAt(wantedPos, Vector3.down);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+        _rigidbody.AddForce(movement.normalized * Time.deltaTime * speed, ForceMode.Force);
+        _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, maxSpeed);
+
+        
+    }
+
+}
