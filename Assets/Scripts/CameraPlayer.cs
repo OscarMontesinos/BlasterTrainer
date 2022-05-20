8538ceeb88e4f518df5974e9629fcc06227e96a0
OscarMontesinos <79588290+OscarMontesinos@users.noreply.github.com>
2022-05-20 11:06:40 +0200

    controlersRobot

---
+++ Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour
+{
+    public float speedH;
+    public float speedV;
+
+    float yaw;
+    float pitch;
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
