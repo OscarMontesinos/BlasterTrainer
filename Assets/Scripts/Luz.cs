507de43071cb21d99b3ae53bf23df9af54b7bd5f
OscarMontesinos <79588290+OscarMontesinos@users.noreply.github.com>
2022-06-02 14:19:20 +0200

    luz

--- Assets/Scripts/Luz.cs
+++ Assets/Scripts/Luz.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Luz : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Material iluminado;
+    public Material apagado;
+    public GameObject luzGo;
+    public float time;
+    float count;
+    bool lighted;
+
+    private void Update()
     {
-        
+        count -= Time.deltaTime;
+        if(count <= 0)
+        {
+            lighted = !lighted;
+            count = time;
+        }
+
+        if (lighted)
+        {
+            luzGo.SetActive(true);
+            GetComponent<MeshRenderer>().material = iluminado;
+        }
+        else
+        {
+
+            luzGo.SetActive(false);
+            GetComponent<MeshRenderer>().material = apagado;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
